ede931efca3a4c7915c6a55e5b91d688e93402c1
xpei <1697505279@qq.com>
2020-10-07 19:26:20 +0800

    删除了私人配置

--- vue.config.js
+++ vue.config.js
@@ -6,12 +6,12 @@ function resolve(dir) {
 module.exports = {
     // productionSourceMap：{ type:Bollean,default:true } 生产源映射，如果您不需要生产时的源映射，那么将此设置为false可以加速生产构建
     productionSourceMap: false,
-    // https://www.my-app.com/。如果应用程序部署在子路径上，则需要使用此选项指定子路径。例如，如果您的应用程序部署在https://www.foobar.com/my-app/，集baseUrl到'/my-app/'.
+    // https://www.my-app.com/。如果应用程序部署在子路径上，则需要使用此选项指定子路径。例如，如果您的应用程序部署在https://www.xxx.com/my-app/，集baseUrl到'/my-app/'.
     publicPath: process.env.NODE_ENV === 'production' ? '/super/' : '/',
     // 环境配置
     devServer: {
         // port: 8085, // 端口号
-        // public: 'http://bk.hosts.run/vue%E5%AD%A6%E4%B9%A0/dist/',
+        // public: 'http://bk.hosts.run/super/dist/',
         open: true, // 配置自动启动浏览器,
         disableHostCheck: true,
         // proxy: 'http://localhost:4000' // 配置跨域处理,只有一个代理
