73b40be3df57db8175675cfd4f8e59843eaf0276
xpei <1697505279@qq.com>
2020-10-07 18:30:33 +0800

    添加了分类，购物车

--- src/network/request.js
+++ src/network/request.js
@@ -1,38 +1,38 @@
 import axios from 'axios'
 import {
-  Toast
+    Toast
 } from "vant";
 
 import Loading from '../store/index'
-// 最终
 export function request(config) {
-  // 1.创建axios实例
-  const instance = axios.create({
-    baseURL: 'http://152.136.185.210:8000/api/w6',
-    timeout: 5000
-  })
+    // 1.创建axios实例
+    const instance = axios.create({
+        // 本接口为假接口,真实接口请添加老师微信coderwhy
+        baseURL: 'xxx',
+        timeout: 5000
+    })
 
-  // 2.拦截器
-  // 2.1请求拦截
-  instance.interceptors.request.use(config => {
-    Loading.state.isLoading && Toast.loading({
-      forbidClick: true,
-      message: "加载中...",
-      loadingType: 'spinner',
-    });
-    return config
-  }, err => {
-    return Promise.reject(err);
-  })
+    // 2.拦截器
+    // 2.1请求拦截
+    instance.interceptors.request.use(config => {
+        Loading.state.isLoading && Toast.loading({
+            forbidClick: true,
+            message: "加载中...",
+            loadingType: 'spinner',
+        });
+        return config
+    }, err => {
+        return Promise.reject(err);
+    })
 
-  // 2.2响应拦截
-  instance.interceptors.response.use(res => {
-    Toast.clear();
-    return res.data
-  }, err => {
-    Toast.clear();
-    return Promise.reject(err);
-  })
-  // 3.发送真正的网络请求
-  return instance(config)
+    // 2.2响应拦截
+    instance.interceptors.response.use(res => {
+        Toast.clear();
+        return res.data
+    }, err => {
+        Toast.clear();
+        return Promise.reject(err);
+    })
+    // 3.发送真正的网络请求
+    return instance(config)
 }
